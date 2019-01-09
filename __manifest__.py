8790022b042e0193c7c8a0f3739da9866bb65dbe
Jesus Hernandez <jehfebles@gmail.com>
2019-01-09 19:06:58 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+{
+    'name': 'Agenda',
+    'summary': """Este modulo nos permitira gestionar una agenda personal""",
+    'version': '0.1',
+    'description': """Agenda de uso personal""",
+    'author': 'Jesus Hernandez',
+    'company': 'hFebles.dev',
+    'website': 'https://github.com/hfebles/agenda',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
